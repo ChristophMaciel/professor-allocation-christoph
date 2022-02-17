ff23593ce3c1784a22f84b3744c02f4b9fa2a9c6
Christoph Maciel <christophmaciel@hotmail.com>
2022-02-17 16:53:38 -0300

    Adicionando projeto inicial

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationChristophApplication.java
@@ -0,0 +1,15 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationChristophApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationChristophApplication.class, args);
+	}
+
+}
+
+
