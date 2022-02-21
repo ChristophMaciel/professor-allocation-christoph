cbc4dfc72a4891391716964488010be0d7918bf7
Christoph Maciel <christophmaciel@hotmail.com>
2022-02-21 17:00:14 -0300

    Mapeamento de colunas

--- src/main/java/com/project/professor/allocation/entity/Course.java
+++ src/main/java/com/project/professor/allocation/entity/Course.java
@@ -1,5 +1,6 @@
 package com.project.professor.allocation.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,8 +10,9 @@ import javax.persistence.Id;
 public class Course {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false, unique = true)
 	private String name;
 
 	public Long getId() {
--- src/main/java/com/project/professor/allocation/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -1,5 +1,6 @@
 package com.project.professor.allocation.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,9 +10,10 @@ import javax.persistence.Id;
 public class Professor {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(length = 14, unique = true, nullable = false)
 	private String cpf;
 	private Long departmentId;
 
