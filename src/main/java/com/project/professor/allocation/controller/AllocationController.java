f1eb7c05e959cec88c1c91d9f75c7593cb6c915e
Christoph Maciel <christophmaciel@hotmail.com>
2022-03-08 15:28:51 -0300

    Class AllocationController

--- src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -1,7 +1,16 @@
 package com.project.professor.allocation.controller;
 
+import java.awt.PageAttributes.MediaType;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.professor.allocation.entity.Allocation;
 import com.project.professor.allocation.service.AllocationService;
 
 @RestController
@@ -12,6 +21,27 @@ public class AllocationController {
 	public AllocationController(AllocationService allocationService) {
 		super();
 		this.allocationService = allocationService;
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(path = "/departments", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Allocation>> findAll() {
+
+		List<Allocation> allocations = allocationService.findAll();
+		return new ResponseEntity<>(allocations, HttpStatus.OK);
+	}
+
+	public ResponseEntity<Allocation> findById(@PathVariable(name = "allocation_id") Long id) {
+
+		Allocation allocation = allocationService.findById(id);
+
+		if (allocation != null) {
+			return new ResponseEntity<>(allocation, HttpStatus.OK);
+
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
 	}
 
 }
