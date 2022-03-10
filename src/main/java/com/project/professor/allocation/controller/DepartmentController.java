0d34af05f8a25f9b76441d6f6c835e3e3f20087b
Christoph Maciel <christophmaciel@hotmail.com>
2022-03-10 18:32:45 -0300

    Classe DepartmentController

--- src/main/java/com/project/professor/allocation/controller/DepartmentController.java
+++ src/main/java/com/project/professor/allocation/controller/DepartmentController.java
@@ -2,16 +2,20 @@ package com.project.professor.allocation.controller;
 
 import java.util.List;
 
+import com.project.professor.allocation.entity.Department;
+import com.project.professor.allocation.service.DepartmentService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.project.professor.allocation.entity.Department;
-import com.project.professor.allocation.service.DepartmentService;
 
 @RestController
 public class DepartmentController {
@@ -26,7 +30,61 @@ public class DepartmentController {
 	@ResponseStatus(HttpStatus.OK)
 	@GetMapping(path = "/departments", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Department>> findAll(@RequestParam(name = "name", required = false) String name) {
-		List<Department> departments = departmentService.findAll();
+		List<Department> departments = departmentService.findAll(name);
 		return new ResponseEntity<>(departments, HttpStatus.OK);
 	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(path = "/{department_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Department> findById(@PathVariable(name = "department_id") Long id) {
+		Department department = departmentService.findById(id);
+		if (department == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(department, HttpStatus.OK);
+		}
+	}
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Department> create(@RequestBody Department department) {
+		try {
+			department = departmentService.create(department);
+			return new ResponseEntity<>(department, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@PutMapping(path = "/{department_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Department> update(@PathVariable(name = "department_id") Long id,
+			@RequestBody Department department) {
+		department.setId(id);
+		try {
+			department = departmentService.update(department);
+			if (department == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				return new ResponseEntity<>(department, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping(path = "/{department_id}")
+	public ResponseEntity<Void> deleteById(@PathVariable(name = "department_id") Long id) {
+		departmentService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping
+	public ResponseEntity<Void> deleteAll() {
+		departmentService.deleteAll();
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
 }
